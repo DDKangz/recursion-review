61e9c7e4a64d77207c339aee39324aba6dda8b72
DDKangz <danieldonghoonkang@gmail.com>
2019-07-15 21:35:42 -0700

    Completed stringfyJson

---
+++ hrsf122-recursion-review/src/stringifyJSON.js
@@ -0,0 +1,54 @@
+// this is what you would do if you liked things to be easy:
+// var stringifyJSON = JSON.stringify;
+
+// but you don't so you're going to write it from scratch:
+
+var stringifyJSON = function(obj) {
+  // your code goes here
+  var stringifyJSON = function(obj) {
+    // checks
+  
+    // let result =;
+    if(typeof obj === 'number' || typeof obj === 'boolean') {
+      return obj.toString();
+    }
+    
+    if (obj === null) {
+      return 'null';
+    }
+    
+    if(typeof obj === 'string') {
+      return '"' + obj + '"';
+    }
+  
+    if (Array.isArray(obj)) {
+      let result = [];
+      if (obj.length === 0) {
+        return '[]';
+      }
+      for(let i = 0 ; i < obj.length; i++) {
+        result.push(stringifyJSON(obj[i]));
+      }
+      return '['+result.toString()+']';
+    }
+  
+    if (typeof obj === 'object') {
+      let result = [];
+  
+      if (Object.keys(obj).length === 0) {
+        return '{}';
+      }
+  
+      for (var key in obj) {
+        if(typeof obj[key] === 'function' || typeof obj[key] === undefined) {
+          return '{}';
+        }
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key])); 
+      }  
+      return '{'+result.toString()+'}';
+    } 
+    
+    // return obj.toString()
+  
+  };
+};
